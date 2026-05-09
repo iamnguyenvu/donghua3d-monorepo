435faae2156ed20b6c131f6fe1783e202d43876d
iamnguyenvu <iamnguyenvu.gm@gmail.com>
2026-05-10 02:00:20 +0700

    fix(prod): fix Next.js image loading remote patterns and Nginx cache routes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,27 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.otruyenapi.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.net',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.org',
+        hostname: '**',
       },
     ],
   },
